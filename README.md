a72bc6c38016d720165e5fed852d05d7e2207f95
pratiksha_B <pratikshabhokare283@gmail.com>
2024-07-21 18:15:40 +0530

    portfolio project files are add

--- README.md
+++
@@ -1,4 +0,0 @@
-# Front-end
-This is my front-end repository which contains the small-basic projects related to frontend technology.
-<br>
-Author - Pratiksha Bhokare
--- app.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelaY: 1000,
-    loop: true
-
-});
